37f712e5ac6f8716fb35b26bf2ef594c12ac877b
Pahrul <ahmdphrl1st14@gmail.com>
2021-05-27 20:55:34 +0800

    Pahrul

---
+++ app/src/main/java/id/ac/unhas/note/NoteViewModel.kt
@@ -0,0 +1,29 @@
+package id.ac.unhas.note
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+
+    private var noteRepository = NoteRepository(application)
+    private var notes: LiveData<List<Note>>? = noteRepository.getNotes()
+
+    fun insertNote(note: Note) {
+        noteRepository.insert(note)
+    }
+
+    fun getNotes(): LiveData<List<Note>>? {
+        return notes
+    }
+
+    fun deleteNote(note: Note) {
+        noteRepository.delete(note)
+    }
+
+    fun updateNote(note: Note) {
+        noteRepository.update(note)
+    }
+
+}
